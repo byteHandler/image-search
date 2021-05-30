eeae9811339c83ebf26dc0b9a4cc5b8e5030981e
byteHandler <jdnikhil65@gmail.com>
2021-05-30 21:20:21 +0530

    page buttons

--- src/components/PageButtonComponent.js
+++ src/components/PageButtonComponent.js
@@ -8,14 +8,14 @@ class PageButtonComponent extends React.Component{
         const buttons = this.state.buttonTexts.map((text,i)=>{
             if(this.state.clickedButtonIndex ===i){
                 return (
-                    <button key = {i} style={{marginLeft:'2px',marginRight:'2px'}} className="ui black button">{text}</button>
+                    <button key = {i} style={{marginLeft:'2px',marginRight:'2px'}} className="ui blue button">{text}</button>
                     )
             }
             return (
             <button key = {i} style={{marginLeft:'2px',marginRight:'2px'}} onClick = {()=>{
                 this.setState({clickedButtonIndex:i})
                 this.props.changeListSize(i+1)
-        }}className="ui blue basic button">{text}</button>
+        }}className="ui secondary basic button">{text}</button>
             )
         })
         return(
