14aee8879beaa9bab0ea16e1e2fb65643cac05ca
byteHandler <jdnikhil65@gmail.com>
2021-05-31 23:30:36 +0530

    pageButtonComponent startIndex fixed

--- src/components/PageButtonComponent.js
+++ src/components/PageButtonComponent.js
@@ -13,17 +13,14 @@ class PageButtonComponent extends React.Component{
         this.setState({clickedButtonIndex:this.state.clickedButtonIndex+1})
     }
     render(){
-        //console.log("clickedButtonIndex :"+this.state.clickedButtonIndex)
-        let startIndex = Math.max((this.state.clickedButtonIndex-(Math.floor(this.state.maxButtons/2))),0)
-        //console.log("start index : "+startIndex)
+        console.log("clickedButtonIndex :"+this.state.clickedButtonIndex)
+        let startIndex = Math.max((this.state.clickedButtonIndex-(Math.floor(this.state.maxButtons/2)))+1,0)
+        console.log("start index : "+startIndex)
         let endIndex = Math.min(startIndex+this.state.maxButtons,(this.state.buttonTexts.length)) 
-        //console.log("end index :"+endIndex)
-        if(startIndex===0){
-            endIndex =  Math.min((this.state.maxButtons),(this.state.buttonTexts.length)) 
-        }
+        console.log("end index :"+endIndex)
         if(endIndex- startIndex < this.state.maxButtons)  {
-            startIndex = Math.min((startIndex-(this.state.buttonTexts.length-(endIndex-startIndex))),0)
-            //console.log("updated start index : ",startIndex)
+            startIndex = Math.max((startIndex-(this.state.maxButtons-(endIndex-startIndex))),0)
+            console.log("updated start index : ",startIndex)
         }
         const buttons = this.state.buttonTexts.slice(startIndex,endIndex).map((text,i)=>{
             if(this.state.clickedButtonIndex ===(Number(text)-1)){
