5126542d150a7185a23fd2899eddb6b383032270
byteHandler <jdnikhil65@gmail.com>
2021-05-31 13:36:46 +0530

    added page numbers

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ class App extends React.Component{
         this.searchInputRef = React.createRef();
         this.imageListRef = React.createRef();
         this.buttonComponentRef = React.createRef();
+        this.pageButtonComponentRef = React.createRef();
     }
     onGridClick = ()=>{
         this.buttonComponentRef.current.setState({gridButton:'blue',listButton:'secondary basic'})
@@ -23,11 +24,18 @@ class App extends React.Component{
     onSearchSubmit= async (entry)=>{
         //console.log(entry)
         const response = await axios.get(`https://pixabay.com/api/?key=21844549-840acc6adaa37fac4e1186c8f&q=${entry.replace(" ","+")}&image_type=photo`)
-        //console.log(response.data.hits)
+        console.log(response)
         this.imageListRef.current.setState({images:response.data.hits,inputValue:entry})
+        //response.data.total is our variable to decide the number
+        const pageSize = 20; // Update whenever needed
+        const nPages = Math.floor(response.data.total/pageSize) + 1
+        const buttonTextArray = Array.from(Array(nPages), (_, index) => index + 1)
+        this.pageButtonComponentRef.current.setState({buttonTexts:buttonTextArray,clickedButtonIndex:0})
     }
-    changeListSize(size){
-        console.log(size)
+    changeImageList= async (pageNo)=>{
+        this.searchInputRef.current.setState({loading:'loading'})
+        const response = await axios.get(`https://pixabay.com/api/?key=21844549-840acc6adaa37fac4e1186c8f&q=${this.searchInputRef.current.state.inputValue.replace(" ","+")}&image_type=photo&page=${pageNo}`)
+        this.imageListRef.current.setState({images:response.data.hits})
     }
     render(){
     return (
@@ -36,7 +44,7 @@ class App extends React.Component{
             <div  className="ui huge header" style={{marginLeft:'auto',marginRight:'auto'}}>Image Search</div>
             <SearchInput ref={this.searchInputRef}  onSearchSubmit={this.onSearchSubmit} inputValue = {this.state.inputValue} loading='category'/>
             <ButtonComponent onGridClick={this.onGridClick} onListClick={this.onListClick} ref={this.buttonComponentRef} defaultButton='grid'/>
-            <PageButtonComponent changeListSize={this.changeListSize} buttonTexts={[1,2,3,4,5,6]} clickedButtonIndex={0} />
+            <PageButtonComponent maxButtons= {7} ref={this.pageButtonComponentRef} changeImageList={this.changeImageList} buttonTexts={[1,2,3]} clickedButtonIndex={0} />
             <ImageList ref={this.imageListRef} searchInputRef={this.searchInputRef}   images={this.state.images} inputValue={this.state.inputValue} view = {this.state.view}/>
         </div>
     )
--- src/components/PageButtonComponent.js
+++ src/components/PageButtonComponent.js
@@ -2,25 +2,49 @@ import React from 'react';
 class PageButtonComponent extends React.Component{
     constructor(props){
         super(props)
-        this.state = {buttonTexts : props.buttonTexts,clickedButtonIndex:props.clickedButtonIndex}
+        this.state = {buttonTexts : props.buttonTexts,clickedButtonIndex:props.clickedButtonIndex,maxButtons:this.props.maxButtons}
+    }
+    onClickPrev=()=>{
+        this.props.changeImageList(this.state.clickedButtonIndex)
+        this.setState({clickedButtonIndex: this.state.clickedButtonIndex-1})
+    }
+    onClickNext=()=>{
+        this.props.changeImageList(this.state.clickedButtonIndex+2)
+        this.setState({clickedButtonIndex:this.state.clickedButtonIndex+1})
     }
     render(){
-        const buttons = this.state.buttonTexts.map((text,i)=>{
-            if(this.state.clickedButtonIndex ===i){
+        const startIndex = Math.max((this.state.clickedButtonIndex-(Math.floor(this.state.maxButtons/2))),0)
+        let endIndex = Math.min((this.state.clickedButtonIndex+Math.floor(this.state.maxButtons)/2),(this.state.buttonTexts.length)) 
+        if(startIndex==0){
+            endIndex =  Math.min((this.state.clickedButtonIndex+this.state.maxButtons),(this.state.buttonTexts.length)) 
+        }
+        /*if(endIndex- startIndex < this.state.maxButtons)  {
+            const temp = this.state.maxButtons - (endIndex-startIndex)
+            endIndex = Math.min(temp,this.state.buttonTexts.length)
+
+        }*/
+        const buttons = this.state.buttonTexts.slice(startIndex,endIndex).map((text,i)=>{
+            if(this.state.clickedButtonIndex ===(Number(text)-1)){
                 return (
-                    <button key = {i} style={{marginLeft:'2px',marginRight:'2px'}} className="ui blue button">{text}</button>
+                    <button key = {i} style={{marginLeft:'2px',marginRight:'2px',width:"2px"}} className="ui blue button">{text}</button>
                     )
             }
             return (
-            <button key = {i} style={{marginLeft:'2px',marginRight:'2px'}} onClick = {()=>{
-                this.setState({clickedButtonIndex:i})
-                this.props.changeListSize(i+1)
+            <button key = {i} style={{marginLeft:'2px',marginRight:'2px',width:"2px"}} onClick = {()=>{
+                this.setState({clickedButtonIndex:Number(text)-1})
+                this.props.changeImageList(Number(text))
         }}className="ui secondary basic button">{text}</button>
             )
         })
         return(
-            <div className="ui icon buttons"style={{marginBottom:"5px"}}>
-                {buttons}
+            <div>
+                <div className="fluid ui icon buttons"style={{marginBottom:"5px"}}>
+                    {buttons}
+                </div> 
+                <div className="fluid ui icon buttons"style={{marginBottom:"5px"}}>
+                    <button onClick={this.onClickPrev} style={this.state.clickedButtonIndex+1 > 1 ? {} : {display:'none'}} className="ui black basic button"><i className="left chevron icon"></i></button>
+                    <button onClick={this.onClickNext} style={this.state.clickedButtonIndex+1 !== this.state.buttonTexts.length ? {} : {display:'none'}} className="ui black basic button"><i className="right chevron icon"></i></button>
+                </div>
             </div>
         )
     }
