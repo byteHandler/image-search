8f01d6547094331e3cb635b0cc7d024196396a60
byteHandler <jdnikhil65@gmail.com>
2021-06-01 22:34:05 +0530

    why

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -14,7 +14,24 @@ class SearchInput extends React.Component{
         return (
             <div className={`ui ${this.state.loading} search`} style={{left:"50%",margin:"0",top:"60%",position:"absolute",transform:"translate(-50%,-50%)"}}>
             <div className="ui icon input">
-                <input onChange={this.onChange} onKeyDown={this.onFormSubmit} style={{borderRightWidth:"5px",borderLeftWidth:"5px",borderLeftColor:"white",borderRightColor:"white",borderLeftStyle:"solid",borderRightStyle:"solid",fontSize:"30px",marginRight:"2px",height:'8vh',width:"40vh",borderBlockWidth:"5px",textDecorationThickness:"3px",borderBlockColor:"white",color:"white",backgroundColor:"transparent"}} className="prompt" type="text" placeholder=""/>
+                <input onChange={this.onChange} onKeyDown={this.onFormSubmit} style={{
+                    borderRightWidth:"3px",
+                    borderLeftWidth:"3px",
+                    borderLeftColor:"white",
+                    borderRightColor:"white",
+                    borderLeftStyle:"solid",
+                    borderRightStyle:"solid",
+                    fontSize:"30px",
+                    marginRight:"2px",
+                    height:'8vh',
+                    width:"40vh",
+                    borderBlockWidth:"3px",
+                    textDecorationThickness:"3px",
+                    borderBlockColor:"white",
+                    color:"white",
+                    backgroundColor:"transparent"}} 
+                    
+                    className="prompt" type="text" placeholder=""/>
                 <i className="inverted search icon"></i>
             </div>
             </div>
