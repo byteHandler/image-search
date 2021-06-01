ac4fc54fb667fc1388f2045c77bfe1af9d73c80a
byteHandler <jdnikhil65@gmail.com>
2021-06-01 21:39:25 +0530

    ui changeover

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -13,7 +13,7 @@ class SearchInput extends React.Component{
     }
     render(){
         return (
-            <div className="ui search" style={{left:"50%",margin:"0",top:"60%",position:"absolute",transform:"translate(-50%,-50%)"}}>
+            <div className={`ui ${this.state.loading} search`} style={{left:"50%",margin:"0",top:"60%",position:"absolute",transform:"translate(-50%,-50%)"}}>
             <div className="ui icon input">
                 <input onChange={this.onChange} onKeyDown={this.onFormSubmit} style={{fontSize:"30px",marginRight:"2px",height:'8vh',width:"40vh",borderBlockWidth:"3px",textDecorationThickness:"3px",borderBlockColor:"white",color:"white",backgroundColor:"transparent"}} className="prompt" type="text" placeholder=""/>
                 <i className="inverted search icon"></i>
