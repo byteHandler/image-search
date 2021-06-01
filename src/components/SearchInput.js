d7dc502214a8b05676187db63b24ec3e1eb2d9f6
byteHandler <jdnikhil65@gmail.com>
2021-06-01 22:32:08 +0530

    why

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -14,7 +14,7 @@ class SearchInput extends React.Component{
         return (
             <div className={`ui ${this.state.loading} search`} style={{left:"50%",margin:"0",top:"60%",position:"absolute",transform:"translate(-50%,-50%)"}}>
             <div className="ui icon input">
-                <input onChange={this.onChange} onKeyDown={this.onFormSubmit} style={{fontSize:"30px",marginRight:"2px",height:'8vh',width:"40vh",borderBlockWidth:"5px",textDecorationThickness:"3px",borderBlockColor:"white",color:"white",backgroundColor:"transparent"}} className="prompt" type="text" placeholder=""/>
+                <input onChange={this.onChange} onKeyDown={this.onFormSubmit} style={{borderRightWidth:"5px",borderLeftWidth:"5px",borderLeftColor:"white",borderRightColor:"white",borderLeftStyle:"solid",borderRightStyle:"solid",fontSize:"30px",marginRight:"2px",height:'8vh',width:"40vh",borderBlockWidth:"5px",textDecorationThickness:"3px",borderBlockColor:"white",color:"white",backgroundColor:"transparent"}} className="prompt" type="text" placeholder=""/>
                 <i className="inverted search icon"></i>
             </div>
             </div>
